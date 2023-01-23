990f87e163fb06d8bef8d4a200e15c9a4e266637
Tiago <70555750+tiagoluis86@users.noreply.github.com>
2023-01-23 10:57:00 -0300

    Delete acamiacxdemo directory

--- acamiacxdemo/src/main/java/com/demo/academiacx/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.demo.academiacx.repository;
-
-import com.demo.academiacx.model.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserModel, Long> {
-
-
-}
